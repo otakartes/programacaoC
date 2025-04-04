4314faa7ce6fd7c08434f36febfd9f7e7a7c9810
otakartes <taiane.digitacao@gmail.com>
2025-04-04 15:14:12 -0300

    Update horadecodar.c

--- horadecodar.c
+++ horadecodar.c
@@ -17,7 +17,7 @@ int main(){
   printf ("Digite sua matricula: \n");
   scanf ("%d", &matricula);
 
-  printf ("Nome do aluno: %s - Matricula: %d", nome, matricula);
+  printf ("Nome do aluno: %s - Matricula: %d \n", nome, matricula);
   printf ("Idade: %d - Altura: %f", idade, altura);
 
 
